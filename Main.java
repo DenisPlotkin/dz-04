359a3bff1898031c92eb37b1c655849368619ff1
Denis Plotkin <79911273+DenisPlotkin@users.noreply.github.com>
2023-04-04 13:20:31 +0300

    Homework05

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Second second = new Second();
+        System.out.println("~~~" + second.print() + "~~~");
+    }
+}
---
+++ Second.java
@@ -0,0 +1,9 @@
+public class Second {
+    int a = 100;
+
+    public String print() {
+        String str = ("---" + a + "---");
+        System.out.print(str);
+        return str;
+    }
+}
